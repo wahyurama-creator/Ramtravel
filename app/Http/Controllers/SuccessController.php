a23d2101ea614be6fefe5adb9e57e941a112e3c2
wahyurama-creator <wahyuramas32@gmail.com>
2021-01-19 19:46:49 +0700

    Add Mail transaction with mjml

--- app/Http/Controllers/SuccessController.php
+++ app/Http/Controllers/SuccessController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\TransactionSuccess;
 use App\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class SuccessController extends Controller
 {
-    public function index(Request $request, int $id) {
-        $transaction = Transaction::findOrFail($id);
+    public function index(Request $request, int $id)
+    {
+        $transaction = Transaction::with(['details', 'travel_package.galleries', 'user'])->findOrFail($id);
         $transaction->transaction_status = 'PENDING';
 
         $transaction->save();
+
+        Mail::to($transaction->user)->send(
+            new TransactionSuccess($transaction)
+        );
+
         return view('pages.success');
     }
 }
